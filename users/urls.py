a3697eeae17bb950acf6f9f0c1d4c8cfcf762ba7
Dayana <dayana.nashkova@yahoo.com>
2023-08-11 15:22:51 +0000

    Create login template and logic #8

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('update-profile', views.update_profile, name='update-profile'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('update-profile/', views.update_profile, name='update-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+
+
+def login_user(request):
+    '''
+    A view that handles login
+    '''
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have successfully logged in')
+            return redirect('home')
+        else:
+            messages.error(request, 'Incorrect login details')
+
+    return render(request, 'users/login.html')
+
+
+def logout_user(request):
+    '''
+    A view that handles user logout
+    '''
+    logout(request)
+    messages.info(request, 'You have logged out')
+    return redirect('home')
 
 
 def update_profile(request):
