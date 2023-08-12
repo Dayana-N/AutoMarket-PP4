1f1d13cb9381a1568f2fcaf3545c5c85b8451ec1
Dayana <dayana.nashkova@yahoo.com>
2023-08-12 20:51:26 +0000

    feat: Create view to handle edit profile form #9

--- users/views.py
+++ users/views.py
@@ -88,6 +88,15 @@ def update_profile(request):
     '''
     profile = request.user.profile
     form = ProfileForm(instance=profile)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid:
+            form.save()
+            return redirect('profile')
+        else:
+            messages.error(request, 'An error has occurred. Try again.')
+
     context = {
         'form': form,
         'profile': profile
