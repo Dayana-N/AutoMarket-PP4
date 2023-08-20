0dabac561b972f609afe7cdddaac633027887aad
Dayana <dayana.nashkova@yahoo.com>
2023-08-20 21:33:23 +0000

    test deployment commit

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,8 @@ const carModelDropdown = document.getElementById('id_car_model');
 
 carMakeDropdown.addEventListener('change', (e) => {
     const makeId = carMakeDropdown.value;
-    const url = `https://8000-dayanan-automarketpp4-018mrjwl7i9.ws-eu104.gitpod.io/load-models/${makeId}`;
+    // const url = `https://8000-dayanan-automarketpp4-018mrjwl7i9.ws-eu104.gitpod.io/load-models/${makeId}`;
+    const url = `https://automarket-2a9033a7b561.herokuapp.com//load-models/${makeId}`;
 
     fetch(url)
     .then(response => response.json())
