8a429a2efcb2d54b8561aae3b4a42b018de54541
Dayana <dayana.nashkova@yahoo.com>
2023-08-23 22:18:37 +0000

    feat: add password reset functionality #10

--- automarket/urls.py
+++ automarket/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,6 +23,17 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('listings.urls')),
     path('users/', include('users.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(
+        template_name='reset_password.html'), name='reset_password'),
+    path('reset_password/sent/', auth_views.PasswordResetDoneView.as_view(
+        template_name='reset_password_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='reset.html'), name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='reset_password_complete.html'),
+         name='password_reset_complete'),
 ]
 # set url for user uploaded content
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
