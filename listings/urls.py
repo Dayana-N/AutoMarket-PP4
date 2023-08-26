980f5f3dabb41643dd4ae349e799cc9acac881a5
Dayana <dayana.nashkova@yahoo.com>
2023-08-26 00:52:51 +0000

    fix: navbar link active condition This fix enables the home link to display as active

--- listings/urls.py
+++ listings/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_page, name='home'),
+    path('', views.home_page, name='home'),
     path('search-listings/', views.search_listings, name='search-listings'),
     path('listings/<str:pk>/', views.single_listing, name='single-listing'),
     path('create-listing/', views.create_listing, name='create-listing'),
