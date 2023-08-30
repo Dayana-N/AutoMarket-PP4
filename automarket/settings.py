0fa1105c7949387dafaa0f46bc11322c433b571b
Dayana <dayana.nashkova@yahoo.com>
2023-08-30 01:17:36 +0000

    chore: prepare for deployment

--- automarket/settings.py
+++ automarket/settings.py
@@ -151,8 +151,8 @@ USE_TZ = True
 
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
--- static/js/index.js
+++ static/js/index.js
@@ -3,9 +3,9 @@ const carModelDropdown = document.getElementById('id_car_model');
 
 carMakeDropdown.addEventListener('change', (e) => {
     const makeId = carMakeDropdown.value;
-    const url = `https://8000-dayanan-automarketpp4-018mrjwl7i9.ws-eu104.gitpod.io/api/car-model/${makeId}`;
+    // const url = `https://8000-dayanan-automarketpp4-018mrjwl7i9.ws-eu104.gitpod.io/api/car-model/${makeId}`;
 
-    // const url = `https://automarket-2a9033a7b561.herokuapp.com/api/car-model/${makeId}`;
+    const url = `https://automarket-2a9033a7b561.herokuapp.com/api/car-model/${makeId}`;
 
     fetch(url)
     .then(response => response.json())
